43dc490fccd1b0c86bf7d6c6e3e1bae97281142f
Adrien Nguyen <knabyss@gmail.com>
2017-10-17 03:23:17 +0200

    change rootUrl from github pages

--- config/environment.js
+++ config/environment.js
@@ -45,7 +45,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/workspace/';
+    ENV.rootURL = '/FortniteTools/';
 
   }
 
